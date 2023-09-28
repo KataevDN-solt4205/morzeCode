3e0478ffbfc69c58f3c8dab9074ea5927af34a51
KataevDN <mingva@yandex.ru>
2023-09-28 14:12:07 +0500

    + добавил разбор параметров и коды выхода

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,72 @@
 #include <stdio.h>
 #include <vector>
 #include <map>
+#include <cstring>
+#include <sysexits.h>
 #include "ICoder.hpp"
 #include "MorzeCoder.hpp"
+#include <getopt.h>
+
+#define DEFAULT_PORT 4902
+
+typedef struct 
+{
+    int port;
+    bool is_server;
+    bool is_client;
+} params_t;
+
+void usage(const char *app_name)
+{
+    printf("%s [-p port] -c / -s\n", app_name);
+    printf("\t-p: socket port\n");
+    printf("\t-c: client\n");
+    printf("\t-s: server\n");
+}
+
+void parse_params(int argc, char *argv[], params_t &params)
+{
+    int ret;
+    memset(&params, 0, sizeof(params_t));
+
+    while ((ret =getopt(argc, argv, "p:sc")) != -1)
+    {
+        switch(ret)
+        {
+            case 'p':
+                params.port = (optarg ? strtol(optarg, NULL, 0) : DEFAULT_PORT);
+                break;
+            case 's':
+                params.is_server = true;
+                break;
+            case 'c':
+                params.is_client = true;
+                break;
+            default:
+                throw std::invalid_argument("Unk. args");
+        }
+    }
+
+    if (params.is_server == params.is_client){
+        throw std::invalid_argument("Unk. args");
+    }
+}
+
 
 int main(int argc, char* argv[])
 {
     /* set ANSII locale*/
 	setlocale(LC_ALL, "C");
+
+    params_t params;
+    try{
+        parse_params(argc, argv, params);
+    }
+    catch (const std::invalid_argument& error)
+    {
+        usage(argv[0]);
+        exit(EX_USAGE);
+    } 
 
     MorzeCoder encoder;
 
@@ -22,5 +81,5 @@ int main(int argc, char* argv[])
     encoder.Encode(src, dst);
     encoder.Decode(dst, outstr);
     std::cout << outstr << std::endl;
-	return 0;
+	exit(EX_OK);
 }
